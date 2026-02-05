9eb083780644681868189984eef644d969f56e3b
Wesley Silva <wesleysi@lsretail.com>
2026-02-05 18:23:46 +0000

    adding pipelines

---
+++ src/SpoolManager.Server/Controllers/VersionController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace SpoolManager.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VersionController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var assembly = typeof(VersionController).Assembly;
+            var fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+            return Ok($"{fileVersionInfo.ProductName} - {fileVersionInfo.FileVersion} - {fileVersionInfo.ProductVersion}");
+        }
+    }
+}
